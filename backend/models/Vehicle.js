3ff2563d089ec4a3f8677fd295d26dcdbff0c175
patrick516 <kulinjipatricks@gmail.com>
2025-05-10 07:57:28 +0200

    impement patching-mark vehicle

--- backend/models/Vehicle.js
+++ backend/models/Vehicle.js
@@ -5,38 +5,21 @@ const vehicleSchema = new mongoose.Schema(
     customerId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Customer",
-      required: false,
       default: null,
     },
-
-    technicianId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Employee",
-      required: false,
-    },
-    supervisorId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Employee",
-      required: false,
-    },
-    plateNumber: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    model: {
-      type: String,
-      required: true,
-    },
+    technicianId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
+    supervisorId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
+    plateNumber: { type: String, required: true, unique: true },
+    model: { type: String, required: true },
     brand: String,
     engineNumber: String,
     color: String,
     notes: String,
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+    createdAt: { type: Date, default: Date.now },
     customerName: String,
+
+    isDone: { type: Boolean, default: false },
+    completedAt: { type: Date, default: null },
   },
   { timestamps: true }
 );
